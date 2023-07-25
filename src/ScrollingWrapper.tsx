6e3b0b29cc86bb1e7d3d80af8a48110afc5c0567
delete101020 <delete101020@live.com>
2023-07-25 10:41:09 +0700

    using windows's scrollbar, instead of element scrollbar

--- src/ScrollingWrapper.tsx
+++ src/ScrollingWrapper.tsx
@@ -1,5 +1,5 @@
 import debounce from 'lodash.debounce';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import React from 'react';
 
 const fetchMoreData = (offset: number) => {
@@ -17,36 +17,24 @@ const ScrollingWrapper = () => {
   const [count, setCount] = useState(0);
   const [data, setData] = useState<number[]>([]); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const [loading, setLoading] = useState(false); // Add the loading state
-  const scrollRef = useRef<HTMLDivElement>(null);
 
   const handleScroll = useCallback(
     debounce(() => {
-      if (scrollRef.current) {
-        const { scrollTop, scrollHeight, clientHeight } = scrollRef.current;
-        if (scrollTop + clientHeight >= scrollHeight - 20 && !loading) {
-          setLoading(true);
-          fetchMoreData(page * 10).then((res: any) => {
-            const { data } = res;
-            setPage((prev) => prev + 1);
-            setCount((prev) => prev + data.length);
-            setData((prev) => [...prev, ...data]);
-            setLoading(false);
-          });
-        }
+      const { scrollTop, scrollHeight, clientHeight } =
+        document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight - 20 && !loading) {
+        setLoading(true); // Set loading to true before fetch
+        fetchMoreData(page * 10).then((res: any) => {
+          const { data } = res;
+          setPage((prev) => prev + 1);
+          setCount((prev) => prev + data.length);
+          setData((prev) => [...prev, ...data]);
+          setLoading(false); // Set loading back to false after fetch is complete
+        });
       }
     }, 200), // Set the debounce wait time (in milliseconds)
-    [page]
+    [page, loading] // Include all dependencies used inside the debounced function
   );
-
-  useEffect(() => {
-    const scrollRefElement = scrollRef.current;
-
-    if (scrollRefElement) {
-      scrollRefElement.addEventListener('scroll', handleScroll);
-
-      return () => scrollRefElement.removeEventListener('scroll', handleScroll);
-    }
-  }, [handleScroll]);
 
   useEffect(() => {
     console.log('Initial load');
@@ -60,32 +48,29 @@ const ScrollingWrapper = () => {
     });
   }, []);
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+
   return (
     <div>
       <h1>Scrolling Wrapper</h1>
       <p>Page: {page}</p>
       <p>Count: {count}</p>
-      <div
-        ref={scrollRef}
-        style={{
-          overflow: 'auto',
-          height: '90vh', // Set the height to less than viewport height
-          border: '1px solid black',
-        }}>
-        <div style={{ height: '110vh' }}>
-          {/* Increase height here for more content */}
-          {data.map((item) => (
-            <p
-              style={{
-                border: '1px solid black',
-                padding: '1rem',
-                margin: '1rem',
-              }}
-              key={item}>
-              {item}
-            </p>
-          ))}
-        </div>
+      <div style={{ height: '110vh' }}>
+        {/* Increase height here for more content */}
+        {data.map((item) => (
+          <p
+            style={{
+              border: '1px solid black',
+              padding: '1rem',
+              margin: '1rem',
+            }}
+            key={item}>
+            {item}
+          </p>
+        ))}
       </div>
     </div>
   );
